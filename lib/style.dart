a02af7667d92127e6282b7d198342a381fc9f85f
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-03 23:33:29 +0600

    Live Test is done

---
+++ lib/style.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:flutter/material.dart';
+
+InputDecoration myInputDecoretion(label){
+  return InputDecoration(
+    hintText: "Title",
+    fillColor: Colors.white70,
+    filled: true,
+    labelText: label,
+    border: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.greenAccent)
+    ),
+  );
+}
+
+TextStyle myTextStyle(){
+  return TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w500
+  );
+}
